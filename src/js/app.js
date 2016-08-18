4514ce44a537305d57b5d23cbdc376f6910106d7
Pinak Shah <pinakshah33@gmail.com>
2016-08-18 17:31:37 +0530

    Fetch locations dynamically through foursquare API and enable filtering locations

--- src/js/app.js
+++ src/js/app.js
@@ -5,75 +5,68 @@ var map_lat = 23.033863, map_lng = 72.585022;
 
 // Initialize the map with default configuration
 function initMap() {
-  // Constructor creates a new map - only center and zoom are required.
-
+  // Creates a new map with ahmedabad as center location
   map = new google.maps.Map(document.getElementById('map'), {
     center: {lat: map_lat, lng: map_lng},
     zoom: 12
   });
 }
 
-var initialLocations = [
-	{
-		name: 'Zencafe',
-		lat: 23.036207442016103,
-		lng: 72.54959106445312
-	},
-	{
-		name: 'Lucky Tea',
-		lat: 23.027900310162142,
-		lng: 72.5813687146695
-	},
-	{
-		name: 'Dunkin Donuts',
-		lat: 23.02471812460175,
-		lng: 72.55643956279857
-	},
-	{
-		name: 'K K tea stall',
-		lat: 23.030214218955297,
-		lng: 72.54064321517944
-	},
-	{
-		name: 'Turquoise Villa',
-		lat: 23.022949135163845,
-		lng: 72.55559045088786
-	},
-	{
-		name: 'SandwichworkZ',
-		lat: 23.02866697207492,
-		lng: 23.02866697207492
-	},
-	{
-		name: 'Cafe Natrani',
-		lat: 23.047684658018554,
-		lng: 72.57267951965332
-	},
-	{
-		name: 'Shambhu\'s Coffee Bar',
-		lat: 23.031933117449288,
-		lng: 72.55507636137939
-	}
-]
-
+// Location Object
 var Location = function(name, lat, lng) {
-	this.name = ko.observable(name);
-	this.lat = lat;
-	this.lng = lng;
+    var self = this;
+    // Location name
+	self.name = ko.observable(name);
+    // Location position
+    self.position = {lat: lat, lng: lng};
+    // Location visible or not
+    self.isVisible = ko.observable(true);
 }
 
+// Model
 var viewModel = function() {
 	var self = this;
 	// List of locations to be displayed
-	this.locationList = ko.observableArray([]);
+	self.locations = ko.observableArray([]);
+	// Query parameter to filter the locations
+	self.query = ko.observable('');
 
-	//Initialize locations
-	initialLocations.forEach(function(locItem){
-		self.locationList.push(new Location(locItem.name, locItem.lat, locItem.lng));
-	});
+	// Fetching coffee shops locations from foursquare API
+    var apiURL = 'https://api.foursquare.com/v2/venues/explore?v=20160819&query=Coffee';
+    apiURL += '&client_id=UPUPOWCSTJIGUEWYOFWHMEJI2J2EDETMWEACC0XXWUGELUAK';
+    apiURL += '&client_secret=H55YZOD5SDS03IQQUU0Z5FRW3XPD4P1343CLDR4VXUOUJLHB';
+    apiURL += '&ll=' + map_lat + ',' +  map_lng;
+
+    $.ajax({
+        url: apiURL,
+        success: function(data) {
+            var items = data.response.groups[0].items;
+            ko.utils.arrayMap(items, function(item) {
+                var venue = item.venue;
+                self.locations.push(new Location(venue.name, venue.location.lat,
+                    venue.location.lng));
+            });
+        },
+        error: function() {
+            $('#locations').append("Failed to get coffee shop locations.");
+        }
+    });
+
+    self.filteredLocations = ko.computed(function() {
+        var query = self.query().toLowerCase();
+        if (!query) {
+            return self.locations();
+        } else {
+            return ko.utils.arrayFilter(self.locations(), function(location) {
+                var isValid = location.name().toLowerCase().indexOf(query) >= 0;
+                location.isVisible(isValid);
+                return isValid;
+            });
+        }
+    });
 
 	// Toggle filter available in the smaller screen
-	this.toggleFilter = function() {
+	self.toggleFilter = function() {
 		jQuery('.row-offcanvas').toggleClass('active')
 	}
 }
