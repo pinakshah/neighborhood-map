ac69f1bcac4cae4835cd50104e53a4cf1d20036a
Pinak Shah <pinakshah33@gmail.com>
2016-08-18 18:19:12 +0530

    Display marker for each location and toggle markers based on filtering location

--- src/js/app.js
+++ src/js/app.js
@@ -1,14 +1,14 @@
 // variable for holding map object
 var map;
 // Latitude and Longitude for center location - Ahmedabad
-var map_lat = 23.033863, map_lng = 72.585022;
+var map_lat = 23.033863, map_lng = 72.555022;
 
 // Initialize the map with default configuration
 function initMap() {
   // Creates a new map with ahmedabad as center location
   map = new google.maps.Map(document.getElementById('map'), {
     center: {lat: map_lat, lng: map_lng},
-    zoom: 12
+    zoom: 13
   });
 }
 
@@ -21,6 +21,13 @@ var Location = function(title, lat, lng) {
     self.position = {lat: lat, lng: lng};
     // Location visible or not
     self.isVisible = ko.observable(true);
+    // Location marker
+    self.marker = new google.maps.Marker({
+        map: map,
+        position: self.position,
+        animation: google.maps.Animation.DROP,
+        title: title
+    });
 }
 
 // Model
@@ -30,6 +37,8 @@ var viewModel = function() {
 	self.locations = ko.observableArray([]);
 	// Query parameter to filter the locations
 	self.query = ko.observable('');
+    // Selected Location
+    this.selectedLocation = ko.observable('Dummy');
 
 	// Fetching coffee shops locations from foursquare API
     var apiURL = 'https://api.foursquare.com/v2/venues/explore?v=20160819&query=Coffee';
@@ -43,6 +52,11 @@ var viewModel = function() {
             var items = data.response.groups[0].items;
             ko.utils.arrayMap(items, function(item) {
                 var venue = item.venue;
+                // Adding click event for location marker
+                google.maps.event.addListener(location.marker, 'click', function() {
+                    self.selectLocation(location);
+                }.bind(location));
+
                 self.locations.push(new Location(venue.name, venue.location.lat,
                     venue.location.lng));
             });
@@ -55,7 +69,10 @@ var viewModel = function() {
     self.filteredLocations = ko.computed(function() {
         var query = self.query().toLowerCase();
         if (!query) {
-            return self.locations();
+            return ko.utils.arrayFilter(self.locations(), function(location) {
+                location.isVisible(true);
+                return true;
+            });;
         } else {
             return ko.utils.arrayFilter(self.locations(), function(location) {
                 var isValid = location.title().toLowerCase().indexOf(query) >= 0;
